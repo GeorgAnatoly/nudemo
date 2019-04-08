a9759e0a3e5fdcf3e4f3ced9e218da9e01464627
blake <finalyetifive@gmail.com>
2019-04-08 14:36:33 -0400

    pre-ToDo form handling

--- src/main/react/com.example.demo/src/App.js
+++ src/main/react/com.example.demo/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import ToDo from "./basicguitodolist/ToDo.js";
 
 class App extends Component {
     render() {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+            <ToDo />
         </header>
       </div>
     );
---
+++ src/main/react/com.example.demo/src/basicguitodolist/ToDo.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class ToDo extends Component {
+
+    render() {
+    }
+
+}
+
+export default ToDo;
