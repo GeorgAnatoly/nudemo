902599c22a2c4f8c876f36121a32f6444696163f
blake <finalyetifive@gmail.com>
2019-04-08 15:07:03 -0400

    adding todo tests

--- src/main/java/com/example/demo/todo/ToDoController.java
+++ src/main/java/com/example/demo/todo/ToDoController.java
@@ -1,13 +1,46 @@
 package com.example.demo.todo;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import com.example.demo.todo.todopersistence.ToDo;
+import com.example.demo.todo.todorepository.ToDoRepository;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
 public class ToDoController {
 
-    @GetMapping("/todos")
-    public String returnTodos() {
-        return "todos.json";
+    private ToDoRepository toDoRepository;
+
+    public ToDoController(ToDoRepository toDoRepository) {
+        this.toDoRepository = toDoRepository;
+    }
+
+    @GetMapping
+    public Iterable findAll() {
+        return toDoRepository.findAll();
+    }
+
+    @GetMapping("/title/{taskTitle}")
+    public List findByTitle(@PathVariable String taskTitle) {
+        return toDoRepository.findByTitle(taskTitle);
+    }
+
+    @GetMapping("/{id}")
+    public ToDo findById(@PathVariable Long id) throws Exception{
+        return toDoRepository.findById(id)
+                .orElseThrow(Exception::new);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) throws Exception {
+        toDoRepository.findById(id)
+                .orElseThrow(Exception::new);
+        toDoRepository.deleteById(id);
+    }
+
+    @PutMapping("/create")
+    public ToDo create(@RequestBody ToDo toDo) {
+        return toDoRepository.save(toDo);
     }
 }
