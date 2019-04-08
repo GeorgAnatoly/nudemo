2805360763038966545cad0eaa1c750412b70f93
blake <finalyetifive@gmail.com>
2019-04-08 13:14:47 -0400

    increment

---
+++ src/main/react/com.example.demo/config-overrides.js
@@ -0,0 +1,12 @@
+module.exports = {
+    webpack: (config, env) => {
+        return config;
+    },
+    jest: config => {
+        return config;
+    },
+    devServer: configFunction => {
+        console.log(configFunction.toString());
+        return configFunction(proxy, allowedHost);
+    }
+};
