32ada5aea9c63491c1a53b5bfdcd22316cdb0245
blake <finalyetifive@gmail.com>
2019-04-11 14:48:13 -0400

    fetch view state works - incorporate dto, repository and client controller

--- src/main/react/com.example.demo/src/todo/modularguielements/userinputforms/ToDoEntryForm.js
+++ src/main/react/com.example.demo/src/todo/modularguielements/userinputforms/ToDoEntryForm.js
@@ -2,6 +2,26 @@ import React, {Component} from 'react';
 
 class ToDoEntryForm extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+                id: 0,
+                title: "",
+                task: ""
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event){
+        let {name, value} = event.target;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
     static handleSubmit(event) {
         event.preventDefault();
     }
@@ -11,12 +31,12 @@ class ToDoEntryForm extends Component {
             <form onSubmit={ToDoEntryForm.handleSubmit}>
                 <label>
                     Title:
-                    <input type="text"/>
+                    <input type="text" name="title" value={this.state.title} onChange={this.handleChange}/>
                 </label>
 
                 <label>
                     Task:
-                    <input type="text"/>
+                    <input type="text" name="task" value={this.state.task} onChange={this.handleChange}/>
                 </label>
 
                 <input type="submit" value="Submit"/>
